345078abc52b7d3b5975ace079bb9f24559e35ba
KarshBa <82422742+KarshBa@users.noreply.github.com>
2025-11-23 15:32:00 -0600

    Update nav.js

--- public/shared/nav.js
+++ public/shared/nav.js
@@ -7,17 +7,23 @@
         <a href="https://inventory-counts.onrender.com/admin_home.html">Home</a>
       </li>
 
-      <li>
+	  <li>
         <a href="#">Quick ▾</a>
         <ul>
           <li>
-            <a href="https://inventory-counts.onrender.com/simplelist.html">List Tool</a>
+            <a href="https://inventory-counts.onrender.com/simplelist.html">
+              List Tool
+            </a>
+          </li>
+		  <li>
+            <a href="https://item-list-handler.onrender.com/item_list.html">
+              Item Search
+            </a>
           </li>
           <li>
-            <a href="https://item-list-handler.onrender.com/item_list.html">Item Search</a>
-          </li>
-          <li>
-            <a href="https://inventory-counts.onrender.com/user_home.html">User Page</a>
+            <a href="https://inventory-counts.onrender.com/user_home.html">
+              User Page
+            </a>
           </li>
         </ul>
       </li>
@@ -26,13 +32,19 @@
         <a href="#">Movement / Sales ▾</a>
         <ul>
           <li>
-            <a href="https://movement-3ka5.onrender.com/item_movement.html">Movement</a>
+            <a href="https://movement-3ka5.onrender.com/item_movement.html">
+              Movement
+            </a>
           </li>
           <li>
-            <a href="https://movement-3ka5.onrender.com/">Movement Admin</a>
+            <a href="https://movement-3ka5.onrender.com/">
+              Movement Admin
+            </a>
           </li>
           <li>
-            <a href="https://movement-3ka5.onrender.com/department_sales.html">Last Week Sales</a>
+            <a href="https://movement-3ka5.onrender.com/department_sales.html">
+              Last Week Sales
+            </a>
           </li>
         </ul>
       </li>
@@ -41,10 +53,14 @@
         <a href="#">Shrink ▾</a>
         <ul>
           <li>
-            <a href="https://inventory-app-bxr3.onrender.com/user.html">Shrink (User)</a>
+            <a href="https://inventory-app-bxr3.onrender.com/user.html">
+              Shrink (User)
+            </a>
           </li>
           <li>
-            <a href="https://inventory-app-bxr3.onrender.com/admin.html">Shrink Admin</a>
+            <a href="https://inventory-app-bxr3.onrender.com/admin.html">
+              Shrink Admin
+            </a>
           </li>
         </ul>
       </li>
@@ -53,46 +69,81 @@
         <a href="#">Price Changes ▾</a>
         <ul>
           <li>
-            <a href="https://sales-batches-app.onrender.com">Price Changes</a>
+            <a href="https://sales-batches-app.onrender.com">
+              Price Changes
+            </a>
           </li>
         </ul>
       </li>
 
-      <li>
+	  <li>
         <a href="#">Purchasing ▾</a>
         <ul>
           <li>
-            <a href="https://hq.geniuscentral.com/login">Genius Central</a>
+            <a href="https://hq.geniuscentral.com/login">
+              Genius Central
+            </a>
           </li>
           <li>
-            <a href="https://myunfi.com/">MyUNFI</a>
+            <a href="https://myunfi.com/">
+              MyUNFI
+            </a>
           </li>
           <li>
-            <a href="https://connect.kehe.com/callback/login">KeHe</a>
+            <a href="https://connect.kehe.com/callback/login">
+              KeHe
+            </a>
           </li>
           <li>
-            <a href="https://www.performancefoodservice.com/Company/Sign-In">Performance</a>
+            <a href="https://www.performancefoodservice.com/Company/Sign-In">
+              Performance
+            </a>
+          </li>
+		  <li>
+            <a href="https://www.ncg.coop/">
+              NCG.coop
+            </a>
+          </li>
+		  <li>
+            <a href="https://ncga-coop.csod.com/login/render.aspx?id=defaultclp">
+              Coop-U
+            </a>
+          </li>
+        </ul>
+      </li>
+	  
+	  <li>
+        <a href="#">Item Maintenance ▾</a>
+        <ul>
+          <li>
+            <a href="https://inventory-counts.onrender.com/open_scale_plu.html">
+              Scale PLU / Scale Plus
+            </a>
+          </li>
+          <li>
+            <a href="https://item-list-handler.onrender.com">
+              Master Item List
+            </a>
           </li>
         </ul>
       </li>
 
-      <li>
+	  <li>
         <a href="#">Inventory ▾</a>
         <ul>
           <li>
-            <a href="https://inventory-counts.onrender.com/user.html">Inventory Counts (User)</a>
+            <a href="https://inventory-counts.onrender.com/user.html">
+              Inventory Counts (User)
+            </a>
           </li>
           <li>
-            <a href="https://inventory-counts.onrender.com/admin.html">Inventory Counts (Admin)</a>
-          </li>
-          <li>
-            <a href="https://inventory-counts.onrender.com/open_scale_plu.html">Scale PLU / Scale Plus</a>
-          </li>
-          <li>
-            <a href="https://item-list-handler.onrender.com/">Master Item List</a>
+            <a href="https://inventory-counts.onrender.com/admin.html">
+              Inventory Counts (Admin)
+            </a>
           </li>
         </ul>
       </li>
+
     </ul>
   </div>
   `.trim();
